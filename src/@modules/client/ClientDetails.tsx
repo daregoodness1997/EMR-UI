694b53cee0cf1706b1b1f331790f41e2ae9ec71d
daregoodness1997 <daregoodness@gmail.com>
2022-08-24 11:24:23 +0100

    started client edit details

--- src/@modules/client/ClientDetails.tsx
+++ src/@modules/client/ClientDetails.tsx
@@ -2,9 +2,13 @@ import { Box, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import DetailView from '../../@views/DetailView';
+import { ButtonGroup, GrayWrapper, HeadWrapper } from '../../@views/styles';
+import { Button } from '../../components';
 import DynamicInput from '../../components/Inputs/DynamicInput';
+import { DetailsWrapper } from '../../components/styles';
+import { DashboardPageWrapper, GridWrapper } from '../../core-ui/styles';
 import { clientsList } from '../../utils/data';
-import { ClientSchema } from '../../utils/schema';
+import { ClientFullSchema, ClientSchema } from '../../utils/schema';
 
 const ClientDetails = () => {
   const { id } = useParams();
@@ -55,9 +59,11 @@ const ClientDetails = () => {
     return null;
   };
 
+  console.log(ClientFullSchema.names);
+
   return (
-    <DetailView title={`${details[0].firstname}`} onEdit={handleEditing}>
-      {keys.map((key, idx) => {
+    <DashboardPageWrapper>
+      {/* {keys.map((key, idx) => {
         // console.log('key', ClientMiniSchema[idx]);
 
         if (isEditing && key !== 'id')
@@ -79,8 +85,239 @@ const ClientDetails = () => {
             <Typography variant='body2'>{`${details[0][key]}`}</Typography>
           </Box>
         );
-      })}
-    </DetailView>
+      })} */}
+      <GrayWrapper>
+        <HeadWrapper>
+          <Box>
+            <Typography variant='h2'>
+              {`${details[0].firstname} ${details[0].lastname}`} Details
+            </Typography>
+            <Typography>
+              Below are your {`${details[0].firstname} ${details[0].lastname}`}{' '}
+              details
+            </Typography>
+          </Box>
+
+          <ButtonGroup>
+            <Button
+              label={'Delete'}
+              background='#FFE9E9'
+              color='#ED0423'
+              showicon={true}
+              icon='bi bi-pen-fill'
+              // onClick={onDelete}
+            />
+            <Button
+              label={`${isEditing ? 'Cancel Editing' : 'Edit Details'}`}
+              background={'#ECF3FF'}
+              color='#0364FF'
+              showicon={true}
+              icon='bi bi-pen-fill'
+              onClick={() => setIsEditing(!isEditing)}
+            />
+          </ButtonGroup>
+        </HeadWrapper>
+        <DetailsWrapper title='Names'>
+          <GridWrapper>
+            {ClientFullSchema.names.map(({ inputType, key, name }) => {
+              return (
+                <>
+                  {isEditing ? (
+                    <DynamicInput
+                      key={key}
+                      label={name}
+                      name={key}
+                      value={details[0][key]}
+                      inputType={inputType}
+                    />
+                  ) : (
+                    <Box key={key}>
+                      <label>{name}</label>
+                      <Typography variant='body2'>{`${details[0][key]}`}</Typography>
+                    </Box>
+                  )}
+                </>
+              );
+            })}
+          </GridWrapper>
+        </DetailsWrapper>
+        <DetailsWrapper title='Biodata'>
+          <GridWrapper>
+            {ClientFullSchema.biodata.map(({ inputType, key, name }) => {
+              return (
+                <>
+                  {isEditing ? (
+                    <DynamicInput
+                      key={key}
+                      label={name}
+                      name={key}
+                      value={details[0][key]}
+                      inputType={inputType}
+                    />
+                  ) : (
+                    <Box key={key}>
+                      <label>{name}</label>
+                      <Typography variant='body2'>{`${details[0][key]}`}</Typography>
+                    </Box>
+                  )}
+                </>
+              );
+            })}
+          </GridWrapper>
+        </DetailsWrapper>
+
+        <DetailsWrapper title='Address'>
+          <GridWrapper>
+            {ClientFullSchema.address.map(({ inputType, key, name }) => {
+              return (
+                <>
+                  {isEditing ? (
+                    <DynamicInput
+                      key={key}
+                      label={name}
+                      name={key}
+                      value={details[0][key]}
+                      inputType={inputType}
+                    />
+                  ) : (
+                    <Box key={key}>
+                      <label>{name}</label>
+                      <Typography variant='body2'>{`${details[0][key]}`}</Typography>
+                    </Box>
+                  )}
+                </>
+              );
+            })}
+          </GridWrapper>
+        </DetailsWrapper>
+
+        <DetailsWrapper title='Other Details'>
+          <GridWrapper>
+            {ClientFullSchema.otherDetails.map(({ inputType, key, name }) => {
+              return (
+                <>
+                  {isEditing ? (
+                    <DynamicInput
+                      key={key}
+                      label={name}
+                      name={key}
+                      value={details[0][key]}
+                      inputType={inputType}
+                    />
+                  ) : (
+                    <Box key={key}>
+                      <label>{name}</label>
+                      <Typography variant='body2'>{`${details[0][key]}`}</Typography>
+                    </Box>
+                  )}
+                </>
+              );
+            })}
+          </GridWrapper>
+        </DetailsWrapper>
+        <DetailsWrapper title='Next of Kin'>
+          <GridWrapper>
+            {ClientFullSchema.nextOfKin.map(({ inputType, key, name }) => {
+              return (
+                <>
+                  {isEditing ? (
+                    <DynamicInput
+                      key={key}
+                      label={name}
+                      name={key}
+                      value={details[0][key]}
+                      inputType={inputType}
+                    />
+                  ) : (
+                    <Box key={key}>
+                      <label>{name}</label>
+                      <Typography variant='body2'>{`${details[0][key]}`}</Typography>
+                    </Box>
+                  )}
+                </>
+              );
+            })}
+          </GridWrapper>
+        </DetailsWrapper>
+        <DetailsWrapper title='Non-Hospital Indentifers'>
+          <GridWrapper>
+            {ClientFullSchema.nonHospitalIndetifiers.map(
+              ({ inputType, key, name }) => {
+                return (
+                  <>
+                    {isEditing ? (
+                      <DynamicInput
+                        key={key}
+                        label={name}
+                        name={key}
+                        value={details[0][key]}
+                        inputType={inputType}
+                      />
+                    ) : (
+                      <Box key={key}>
+                        <label>{name}</label>
+                        <Typography variant='body2'>{`${details[0][key]}`}</Typography>
+                      </Box>
+                    )}
+                  </>
+                );
+              }
+            )}
+          </GridWrapper>
+        </DetailsWrapper>
+        <DetailsWrapper title='Payment Information'>
+          <GridWrapper>
+            {ClientFullSchema.paymentInformation.map(
+              ({ inputType, key, name }) => {
+                return (
+                  <>
+                    {isEditing ? (
+                      <DynamicInput
+                        key={key}
+                        label={name}
+                        name={key}
+                        value={details[0][key]}
+                        inputType={inputType}
+                      />
+                    ) : (
+                      <Box key={key}>
+                        <label>{name}</label>
+                        <Typography variant='body2'>{`${details[0][key]}`}</Typography>
+                      </Box>
+                    )}
+                  </>
+                );
+              }
+            )}
+          </GridWrapper>
+        </DetailsWrapper>
+
+        <DetailsWrapper title='Medical Data'>
+          <GridWrapper>
+            {ClientFullSchema.medicalData.map(({ inputType, key, name }) => {
+              return (
+                <>
+                  {isEditing ? (
+                    <DynamicInput
+                      key={key}
+                      label={name}
+                      name={key}
+                      value={details[0][key]}
+                      inputType={inputType}
+                    />
+                  ) : (
+                    <Box key={key}>
+                      <label>{name}</label>
+                      <Typography variant='body2'>{`${details[0][key]}`}</Typography>
+                    </Box>
+                  )}
+                </>
+              );
+            })}
+          </GridWrapper>
+        </DetailsWrapper>
+      </GrayWrapper>
+    </DashboardPageWrapper>
   );
 };
 
